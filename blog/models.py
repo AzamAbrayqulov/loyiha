f6cf1ab0a40a858c4e5714bdcbcacf55be423b89
Azam <azamabrayqulov00@gmail.com>
2022-03-17 14:53:01 +0500

    m

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+from turtle import title
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE
+    )
+    # summary = models.CharField(max_length=400)
+    body = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("post_detail", args=[str(self.pk)])
+     
---
+++ blog/views.py
@@ -0,0 +1,32 @@
+from dataclasses import fields
+from pyexpat import model
+from re import template
+from sre_constants import SUCCESS
+from django.shortcuts import render
+from django.views.generic import ListView,DetailView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
+from .models import Post
+
+# Create your views here.
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class BlogCreatView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title','author','body']
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title','body']
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
+
